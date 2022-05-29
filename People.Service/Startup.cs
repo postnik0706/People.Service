f1afbbc0b9590c9bc9b701c7d593caf30a3f6b4c
postnik0706 <postnik0706@hotmail.com>
2022-05-29 16:23:27 -0400

    Moving to .Net 6.0

--- People.Service/Startup.cs
+++ People.Service/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using People.Service.Models;
 
 namespace People.Service
@@ -19,12 +20,15 @@ namespace People.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(setupAction =>
+            {
+                setupAction.EnableEndpointRouting = false;
+            });
             services.AddSingleton<IPeopleProvider, StaticPeopleProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
