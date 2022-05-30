2e38a58cc5d17e5f44d58f109f0451c05d669eb4
postnik0706 <postnik0706@hotmail.com>
2022-05-30 13:11:56 -0400

    Ninject container.

---
+++ PeopleViewer.Ninject/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Ninject;
+using PeopleViewer.Common;
+using PersonDataReader.Decorators;
+using PersonDataReader.Service;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+        IKernel Container = new StandardKernel();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            Current.MainWindow.Title = "With DI - Ninject";
+            Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            Container.Bind<IPersonReader>().To<CachingReader>()
+                .InSingletonScope()
+                .WithConstructorArgument<IPersonReader>(Container.Get<ServiceReader>());
+        }
+
+        private void ComposeObjects()
+        {
+            Current.MainWindow = Container.Get<PeopleViewerWindow>();
+        }
+    }
+}
