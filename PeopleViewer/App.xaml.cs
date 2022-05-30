48a7e3b12d0e82e3ee07c87769f358ee87aec384
postnik0706 <postnik0706@hotmail.com>
2022-05-29 23:09:35 -0400

    Adding a cache/decorator object.

--- PeopleViewer/App.xaml.cs
+++ PeopleViewer/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using PeopleViewer.Presentation;
 using PersonDataReader.CSV;
+using PersonDataReader.Decorators;
 using PersonDataReader.Service;
 using PersonDataReader.SQL;
 using System.Windows;
@@ -18,8 +19,9 @@ namespace PeopleViewer
 
         private static void ComposeObjects()
         {
-            var reader = new SQLReader();
-            var viewModel = new PeopleViewModel(reader);
+            var reader = new ServiceReader();
+            var cachingReader = new CachingReader(reader);
+            var viewModel = new PeopleViewModel(cachingReader);
             Application.Current.MainWindow = new PeopleViewerWindow(viewModel);
         }
     }
