16aa54cdebb59647c20871f5e83e9bb3f39dd679
postnik0706 <postnik0706@hotmail.com>
2022-06-04 19:21:32 -0400

    Adding Autofac and late binding

---
+++ PeopleViewer.Autofac/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using Autofac.Features.ResolveAnything;
+using PeopleViewer.Common;
+using PeopleViewer.Presentation;
+using PersonDataReader.CSV;
+using PersonDataReader.Decorators;
+using PersonDataReader.Service;
+using System.Windows;
+
+namespace PeopleViewer.Autofac
+{
+    public partial class App : Application
+    {
+        IContainer Container;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            Application.Current.MainWindow.Title = "With Dependency Injection - Autofac";
+            Application.Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<CSVReader>().Named<IPersonReader>("reader").SingleInstance();
+            builder.RegisterDecorator<IPersonReader>((c, inner) => new CachingReader(inner), fromKey: "reader");
+
+            builder.RegisterType<PeopleViewerWindow>().InstancePerDependency();
+            builder.RegisterType<PeopleViewModel>().InstancePerDependency();
+
+            Container = builder.Build();
+        }
+
+        private void ComposeObjects()
+        {
+            Application.Current.MainWindow = Container.Resolve<PeopleViewerWindow>();
+        }
+    }
+}
